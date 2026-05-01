9fa6a93af638e4e685353b77341bf7bfc43d710e
itamarHenrique <itamarhjp1@gmail.com>
2026-05-01 17:06:55 -0300

    fix: correção de erro do require

--- config/database.php
+++ config/database.php
@@ -1,14 +1,21 @@
 <?php
 
-$host = '';
-$db = '';
-$username = '';
-$password = '';
+$host = '127.0.0.1';
+$db = 'w5i_chamados';
+$username = 'root';
+$password = '0515';
+$charset = 'utf8mb4';
+
+$dataSourceName = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
 
 try{
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+    $pdo = new PDO($dataSourceName, $username, $password, $options);
 }catch(PDOException $e){
     echo 'Erro na conexão: ' . $e->getMessage();
 }
--- src/Controller/chamadaController.php
+++ src/Controller/chamadaController.php
@@ -1,5 +1,5 @@
 <?php
-require_once '/../config/database.php';
+require_once __DIR__ . '\../config/database.php';
 
 $acao = $_POST['acao'] ?? $_GET['acao'] ?? null;
 
