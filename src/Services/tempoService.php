600230970856b067f2cff5de7c93932b7ba97ba3
itamarHenrique <itamarhjp1@gmail.com>
2026-05-01 16:08:15 -0300

    feat: adição da logica para o calculo de tempo

---
+++ src/Services/tempoService.php
@@ -0,0 +1,37 @@
+<?php
+
+function calcularTempo($dataInicio, $dataFim, $tempoEstimadoPorHora){
+    
+    if (!$dataInicio) {
+        return [
+            'formatado' => '---',
+            'estourou' => false
+        ];
+    }
+
+    $inicio = new DateTime($dataInicio);
+
+    if ($dataFim) {
+        $fim = new DateTime($dataFim);
+    } else {
+        $fim = new DateTime();
+    }
+
+    $intervalo = $inicio->diff($fim);
+
+
+    $horasTotaisUsadas = ($intervalo->days * 24) + $intervalo->h + ($intervalo->i / 60);
+
+    $estourou = $horasTotaisUsadas >= $tempoEstimadoPorHora;
+
+    if ($intervalo->days > 0){
+        $formatado = $intervalo->format('%a dias, %h horas e %i minutos');
+    }else{
+        $formatado = $intervalo->format('%h horas e %i minutos');
+    }
+
+    return [
+        'formatado' => $formatado,
+        'estourou' => $estourou
+    ];
+}
